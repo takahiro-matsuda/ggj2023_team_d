d309ef3b442a6fc51a2dfada1af660e1e799e1f3
takahiro-matsuda <takahir.matsuda@gmail.com>
2023-02-05 14:14:54 +0900

    リザルト画面を実装

---
+++ GGJ2023/Assets/Scenes/Script/NormClearEnd.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NormClearEnd : MonoBehaviour
+{
+    public AudioSource se1;
+
+    // 開始
+    void Start()
+    {
+        Invoke("playSe1", 1.0f);
+    }
+
+    // 更新
+    void Update()
+    {
+        // スペースキーが押されたらシーン遷移
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title2");
+        }
+    }
+
+    // SE1再生
+    private void playSe1() {
+        se1.Play();
+    }
+}
